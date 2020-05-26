019413403caeab260dd20f03f06dc4afe3d6c0d6
AsherSeiling <f14asher@gmail.com>
2020-05-26 16:01:48 -0500

    Java and C++ files to convert pi to new pi and .gitignore

---
+++ Convert different pi numbers to new pi/Old_pi_to_new_pi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Old_pi_to_new_pi{
+    public static void main(final String[] args) {
+        // Scanner Decleration
+        Scanner input = new Scanner(System.in);
+
+        // First displayed line
+        System.out.println("Input the variarion of pi you want to convert to new pi");
+        // Input under the line
+        Double pi_num_var = input.nextDouble();
+        
+    } 
+}
